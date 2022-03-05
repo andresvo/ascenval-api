ba4b5c5147de7cd5457b8a94150fc2011764eff8
Andrés V <andres@valsis.cl>
2022-03-05 19:08:21 -0300

    Prueba nodemailer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import bodyParser from "body-parser";
 import express from "express";
 
+let nodemailer = require('nodemailer')
+
 const app = express();
 const port = process.env.PORT || 3333;
 
@@ -9,6 +11,30 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
+	const transporter = nodemailer.createTransport({
+		port: 587,
+		host: "smtp.sparkpostmail.com",
+		auth: {
+		  user: 'SMTP_Injection',
+		  pass: process.env.SPARKPOST_API_KEY,
+		},
+		secure: true,
+	})
+
+	const mailData = {
+		from: 'contacto@ascenval.cl',
+		to: 'andresvalenzuelao@gmail.com',
+		subject: `Message From Next.js`,
+		text: 'prueba',
+		html: '<div>prueba</div>'
+	}
+
+	transporter.sendMail(mailData, function (err: any, info: any) {
+		if(err)
+			console.log(err)
+		else
+			console.log(info)
+	})
   res.json({ Hello: "World" });
 });
 
